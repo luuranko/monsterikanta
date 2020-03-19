2d44216c64d44c6f1b8b19579e12ffb8ca94237a
Lauri Halttunen <lauri.halttunen@helsinki.fi>
2020-03-19 17:24:49 +0200

    osa 2, kohta 2.2.

--- application/__init__.py
+++ application/__init__.py
@@ -1,22 +1,14 @@
-# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
-# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
-# Käytetään monsters.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///monsters.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
 app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
+from application.monsters import models
 
-# Luodaan lopulta tarvittavat tietokantataulut
 db.create_all()
--- application/monsters/models.py
+++ application/monsters/models.py
@@ -11,48 +11,48 @@ class Monster(db.Model):
 # Monsterin julkisuusasetus
     public = db.Column(db.Boolean, nullable=False)
 # Monsterin tyyppi, myöhemmin muutetaan enum-muotoon Stringin sijaan
-#    mtype = db.Column(db.String(144), nullable=False)
+    mtype = db.Column(db.String(144), nullable=False)
 # Monsterin koko, myöhemmin muutetaan enum-muotoon Stringin sijaan
-#    size = db.Column(db.String(144), nullable=False)
+    size = db.Column(db.String(144), nullable=False)
 # Monsterin haastetaso, myöhemmin muutetaan enum-muotoon Integerin sijaan
-#    cr = db.Column(db.Integer, nullable=False)
-# Monsterin heikkoudet listana, joka syötetään String-muodossa. Voi olla null
-#    weakto = db.Column(db.String(144), nullable=True)
-# Monsterin vahvuudet listana, joka syötetään String-muodossa. Voi olla null
-#    resist = db.Column(db.String(144), nullable=True)
+    cr = db.Column(db.Integer, nullable=False)
+# Monsterin heikkoudet listana, joka syötetään String-muodossa
+    weakto = db.Column(db.String(144), nullable=False)
+# Monsterin vahvuudet listana, joka syötetään String-muodossa 
+    resist = db.Column(db.String(144), nullable=False)
 # Monsterin sanallinen kuvaus
-#    descrip = db.Column(db.String(1000), nullable=False)
+    descrip = db.Column(db.String(144), nullable=False)
 # Monsterin Hit Pointit
-#    hp = db.Column(db.Integer, nullable=False)
+    hp = db.Column(db.Integer, nullable=False)
 # Monsterin Armor Class
-#    ac = db.Column(db.Integer, nullable=False)
+    ac = db.Column(db.Integer, nullable=False)
 # Monsterin Strength-stat
-#    stre = db.Column(db.Integer, nullable=False)
+    stre = db.Column(db.Integer, nullable=False)
 # Monsterin Dexterity-stat
-#    dex = db.Column(db.Integer, nullable=False)
+    dex = db.Column(db.Integer, nullable=False)
 # Monsterin Constitution-stat
-#    con = db.Column(db.Integer, nullable=False)
+    con = db.Column(db.Integer, nullable=False)
 # Monsterin Intelligence-stat
-#    inte = db.Column(db.Integer, nullable=False)
+    inte = db.Column(db.Integer, nullable=False)
 # Monsterin Wisdom-stat
-#    wis = db.Column(db.Integer, nullable=False)
+    wis = db.Column(db.Integer, nullable=False)
 # Monsterin Charisma-stat
-#    cha = db.Column(db.Integer, nullable=False)
+    cha = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, name):
+    def __init__(self, name, mtype, size, cr, weakto, resist, descrip, hp, ac, stre, dex, con, inte, wis, cha):
         self.name = name
         self.public = False
-#        self.mtype = mtype
-#        self.size = size
-#        self.cr = cr
-#        self.weakto = weakto
-#        self.resist = resist
-#        self.descrip = descrip
-#        self.hp = hp
-#        self.ac = ac
-#        self.stre = stre
-#        self.dex = dex
-#        self.con = con
-#        self.inte = inte
-#        self.wis = wis
-#        self.cha = cha
+        self.mtype = mtype
+        self.size = size
+        self.cr = cr
+        self.weakto = weakto
+        self.resist = resist
+        self.descrip = descrip
+        self.hp = hp
+        self.ac = ac
+        self.stre = stre
+        self.dex = dex
+        self.con = con
+        self.inte = inte
+        self.wis = wis
+        self.cha = cha
